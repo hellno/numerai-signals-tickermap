871ce34f357aa770550c77a5cd0976a54dfa4ec2
hellno <holgerniessner@me.com>
2021-02-17 15:01:59 +0100

    refactor alpha_vantage_tickers using logic from @jonrtaylor in #2 update ticker_map.csv remove dependency on alpha_vantage api

--- scripts/alpha_vantage_tickers.py
+++ scripts/alpha_vantage_tickers.py
@@ -2,120 +2,115 @@
 Read old ticker mapping from S3 and add alpha_vantage column.
 Script might need to re-run because of API timeout after daily request limit (only for free plans)
 """
-import os
-from time import sleep
+from typing import Optional
 
+import numerapi
 import pandas as pd
-from alpha_vantage.timeseries import TimeSeries
-from tqdm import tqdm
-
-tqdm.pandas()
 
 TICKER_MAP_URL = 'https://numerai-signals-public-data.s3-us-west-2.amazonaws.com/signals_ticker_map_w_bbg.csv'
-ALPHA_VANTAGE_API_KEY = 'YOUR_API_KEY'
 TICKER_MAP_FNAME = 'ticker_map.csv'
-
-ALPHA_VANTAGE_TICKER_COLUMN = '1. symbol'
-SYMBOL_MATCH_CONFIDENCE_THRESHOLD = 0.9
-TIMEOUT_SLEEP_SEC = 20
-TIMEOUT = 'TIMEOUT'
 SYMBOL_NOT_FOUND = 'SYMBOL_NOT_FOUND'
 
-has_api_timeout = False
-print(f'initialize alpha vantage api')
-ts = TimeSeries(key=ALPHA_VANTAGE_API_KEY, output_format='pandas')
+UNSUPPORTED_MARKET_SUFFIXES = [
+    ' PW', ' CP', ' TB', ' HB', ' IT', ' ID', ' PM',
+    ' SP', ' FH', ' AV', ' GA', ' DC', ' NZ', ' TI',
+    ' NO', ' SM', ' IJ', ' MF', ' SJ', ' AU', ' SW',
+    ' SS', ' KS', ' MK', ' IM', ' JP', ' TT', ' HK',
+]
 
 
-def get_ticker_info(ticker: str):
-    try:
-        data, _ = ts.get_symbol_search(ticker)
-    except ValueError as err:
-        print(f'got err from alpha vantage: {err}')
-        return TIMEOUT
+def is_available_on_alpha_vantage(ticker: str) -> bool:
+    return not any(ticker.endswith(market_suffix) for market_suffix in UNSUPPORTED_MARKET_SUFFIXES)
 
-    if len(data) == 0:
+
+def get_alpha_vantage_suffix_for_bloomberg_ticker(ticker: str) -> Optional[str]:
+    if ticker.endswith(' US'):
+        return ''
+    elif ticker.endswith(' NA'):
+        return '.AMS'
+    elif ticker.endswith(' CA'):
+        return '.TRT'
+    elif ticker.endswith(' BB'):
+        return '.BRU'
+    elif ticker.endswith(' FP'):
+        return '.PAR'
+    elif ticker.endswith(' GR'):
+        return '.DEX'
+    elif ticker.endswith(' PL'):
+        return '.LIS'
+    elif ticker.endswith(' LN'):
+        return '.LON'
+    elif ticker.endswith(' BZ'):
+        return '.SAO'
+    else:
+        return None
+
+
+def convert_bloomberg_ticker_to_alpha_vantage_format(ticker: str) -> str:
+    if ticker.endswith('/A.LON'):
+        ticker = ticker.replace('/', '-')
+    if ticker.endswith('LON'):
+        ticker = ticker.replace('/', '')
+        ticker = ticker.replace('*', '')
+    if '/' in ticker:
+        ticker = ticker.replace('/', '-')
+    return ticker
+
+
+def get_alpha_vantage_ticker_for_row(row: pd.Series) -> str:
+    if not row['is_available_on_alpha_vantage']:
         return SYMBOL_NOT_FOUND
 
-    symbol_candidate = data.iloc[0].to_dict()
-    symbol_match_score = float(symbol_candidate['9. matchScore'])
-    is_confident_candidate = symbol_match_score >= SYMBOL_MATCH_CONFIDENCE_THRESHOLD
+    bloomberg_ticker = row['bloomberg_ticker']
+    ticker = row['ticker']
 
-    return symbol_candidate if is_confident_candidate else SYMBOL_NOT_FOUND
-
-
-def get_ticker_info_with_retry(ticker: str, retry_count: int = 4):
-    retries = 0
-    symbol = None
-
-    while retries < retry_count:
-        symbol = get_ticker_info(ticker)
-        is_api_timeout = symbol == TIMEOUT
-        if is_api_timeout:
-            retries += 1
-            sleep(TIMEOUT_SLEEP_SEC)
-        else:
-            break
-
-    return symbol
+    suffix = get_alpha_vantage_suffix_for_bloomberg_ticker(bloomberg_ticker)
+    if suffix is not None:
+        ticker = ticker + suffix
+        ticker = convert_bloomberg_ticker_to_alpha_vantage_format(ticker)
+    else:
+        ticker = SYMBOL_NOT_FOUND
+    return ticker
 
 
-def get_alpha_vantage_ticker_symbol_for_row(row, verbose: bool = True):
-    global has_api_timeout
+def get_public_numerai_ticker_map() -> pd.DataFrame:
+    # current public ticker map includes bloomberg to yahoo tickers
 
-    if row.alpha_vantage is not None and row.alpha_vantage != TIMEOUT:
-        return row.alpha_vantage
+    ticker_map = pd.read_csv(TICKER_MAP_URL)
+    ticker_map.drop('ticker', axis=1, inplace=True)
 
-    if has_api_timeout:
-        return TIMEOUT
-
-    yahoo_ticker = row.yahoo
-    if verbose:
-        print(f'getting info for ticker: {yahoo_ticker}')
-
-    ticker_info = get_ticker_info_with_retry(yahoo_ticker)
-    if verbose:
-        print(f'response: {ticker_info}')
-
-    if ticker_info == SYMBOL_NOT_FOUND:
-        return ticker_info
-    elif ticker_info == TIMEOUT:
-        print(f'got timeout after retries, might be temporarily blocked from API access')
-        has_api_timeout = True
-        return TIMEOUT
-
-    return ticker_info[ALPHA_VANTAGE_TICKER_COLUMN]
+    ticker_map['yahoo'] = ticker_map['yahoo'].str.replace('/.', '.', regex=False)
+    ticker_map.loc[ticker_map['yahoo'] == 'BT/A.L', 'yahoo'] = 'BT-A.L'
+    return ticker_map
 
 
-def get_alpha_vantage_ticker_symbols(ticker_map):
-    global has_api_timeout
-    has_api_timeout = False
-
-    ticker_map['alpha_vantage'] = ticker_map.progress_apply(get_alpha_vantage_ticker_symbol_for_row, axis=1)
-    if has_api_timeout:
-        ticker_timeout_count = len(ticker_map[ticker_map.alpha_vantage == TIMEOUT])
-        print(f'did not finish all tickers due to API timeout ({ticker_timeout_count} tickers remaining)')
+def print_ticker_map_stats(ticker_map: pd.DataFrame):
+    for column in ticker_map.columns:
+        nr_tickers_not_found = len(ticker_map[ticker_map[column] == SYMBOL_NOT_FOUND])
+        coverage_percentage = 100 * (1 - nr_tickers_not_found / len(ticker_map))
+        info = f'({nr_tickers_not_found} of {len(ticker_map)} tickers unavailable)' if nr_tickers_not_found else ''
+        print(f'{column} covers {coverage_percentage:.2f}% {info}')
 
 
 def main():
-    new_ticker_map_file_exists = os.path.exists(TICKER_MAP_FNAME)
-    if new_ticker_map_file_exists:
-        print(f'using local ticker map from file {TICKER_MAP_FNAME}')
-        ticker_map = pd.read_csv(TICKER_MAP_FNAME)
-    else:
-        print(f'using local ticker map from file {TICKER_MAP_FNAME}')
-        ticker_map = pd.read_csv(TICKER_MAP_URL)
-    if 'alpha_vantage' not in ticker_map.columns:
-        ticker_map['alpha_vantage'] = None
+    numerai_tickers = pd.DataFrame(numerapi.SignalsAPI().ticker_universe(), columns=['bloomberg_ticker'])
+    public_ticker_map = get_public_numerai_ticker_map()
+    unique_bloomberg_tickers = list(set(numerai_tickers.bloomberg_ticker.tolist() +
+                                        public_ticker_map.bloomberg_ticker.tolist()))
 
-    get_alpha_vantage_ticker_symbols(ticker_map)
-    nr_alpha_vantage_tickers = len(ticker_map[
-                                       ~ticker_map.alpha_vantage.isna() &
-                                       (ticker_map.alpha_vantage != TIMEOUT) &
-                                       (ticker_map.alpha_vantage != SYMBOL_NOT_FOUND)
-                                       ])
-    print(f'Alpha Vantage tickers available: {nr_alpha_vantage_tickers} '
-          f'({nr_alpha_vantage_tickers / len(ticker_map) * 100:.2f}%)')
+    print(f'Tickers in current Numerai Signals tournament: {len(numerai_tickers)}')
+    print(f'Tickers in public numerai mapping: {len(public_ticker_map)}')
+    print(f'Total number of unique bloomberg tickers: {len(unique_bloomberg_tickers)}')
 
+    ticker_map = pd.DataFrame({'bloomberg_ticker': unique_bloomberg_tickers, 'alpha_vantage': None})
+    ticker_map['ticker'] = ticker_map.bloomberg_ticker.str[:-3]
+    ticker_map['is_available_on_alpha_vantage'] = ticker_map.bloomberg_ticker.apply(is_available_on_alpha_vantage)
+    ticker_map['alpha_vantage'] = ticker_map.apply(get_alpha_vantage_ticker_for_row, axis=1)
+    ticker_map.drop(['ticker', 'is_available_on_alpha_vantage'], axis=1, inplace=True)
+
+    ticker_map = ticker_map.merge(public_ticker_map, how='left', on='bloomberg_ticker')
+    ticker_map.yahoo.fillna(SYMBOL_NOT_FOUND, inplace=True)
+    print_ticker_map_stats(ticker_map)
     ticker_map.to_csv(TICKER_MAP_FNAME, index=False)
     print(f'done - saved tickers to file {TICKER_MAP_FNAME}')
 
